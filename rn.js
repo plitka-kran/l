261b956d1ef054514b015269b2f43c1a1cec78bd
plitka-kran <plitka.kran@gmail.com>
2025-10-03 09:25:55 +0300

    Update rn.js

--- rn.js
+++ rn.js
@@ -10,7 +10,7 @@
     });
 
     window.lampa_settings.torrents_use = true;
-    Lampa.Utils.putScriptAsync(['https://lampame.github.io/main/pubtorr/pubtorr.js'], function () { });
+    Lampa.Utils.putScriptAsync(['https://plitka-kran.github.io/l/pubtorr.js'], function () { });
     
     function startsWith(str, searchString) {
       return str.lastIndexOf(searchString, 0) === 0;
@@ -14668,6 +14668,7 @@
     });
 
 })();
+
 
 
 
