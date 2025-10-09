722f8bfb26f463fd468a956732be4c9d140b47a4
plitka-kran <plitka.kran@gmail.com>
2025-10-09 23:07:19 +0300

    Update rn.js

--- rn.js
+++ rn.js
@@ -11,7 +11,7 @@
 
     window.lampa_settings.torrents_use = true;
     Lampa.Utils.putScriptAsync(['https://plitka-kran.github.io/l/pubtorr.js'], function () { });
-    Lampa.Utils.putScriptAsync(['https://cub.red/plugin/collections'], function () { });
+    Lampa.Utils.putScriptAsync(['http://llpp.in/v/vod.js'], function () { });
     
     function startsWith(str, searchString) {
       return str.lastIndexOf(searchString, 0) === 0;
@@ -14669,6 +14669,7 @@
     });
 
 })();
+
 
 
 
