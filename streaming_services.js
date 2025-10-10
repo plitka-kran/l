de9b43cb6bf57ed512e8d01fc0994edd72a26be0
plitka-kran <plitka.kran@gmail.com>
2025-10-10 12:47:54 +0300

    Update streaming_services.js

--- streaming_services.js
+++ streaming_services.js
@@ -1,140 +1,89 @@
 // Расширение для Лампы - Стриминговые сервисы
-// Добавляет категории в левое меню
-// Версия: 3.0
+// Версия: 4.0 - Совместимость со всеми версиями Lampa
 
 (function() {
     'use strict';
 
-    var Plugin = {
+    var manifest = {
+        type: 'video',
+        version: '1.0.0',
         name: 'Стриминговые сервисы',
-        version: '3.0'
+        description: 'Netflix, HBO, Hulu и другие популярные сервисы',
+        component: 'streaming_services'
     };
 
-    // Конфигурация стриминговых сервисов
-    var STREAMING_SERVICES = {
-        netflix: {
-            name: 'Netflix',
-            id: 8,
-            icon: '<svg width="26" height="26" viewBox="0 0 24 24" fill="#E50914"><path d="M5.398 0v.006c3.028 8.556 5.37 15.175 8.348 23.994 2.344.056 4.85.398 7.254.4-3.462-9.547-5.923-16.219-9.15-24.4H5.398z"/></svg>'
-        },
-        hbo: {
-            name: 'HBO Max',
-            id: 384,
-            icon: '<svg width="26" height="26" viewBox="0 0 24 24" fill="#9c27b0"><circle cx="12" cy="12" r="10"/></svg>'
-        },
-        hulu: {
-            name: 'Hulu',
-            id: 15,
-            icon: '<svg width="26" height="26" viewBox="0 0 24 24" fill="#3DBB3D"><rect width="24" height="24" rx="4"/></svg>'
-        },
-        disney: {
-            name: 'Disney+',
-            id: 337,
-            icon: '<svg width="26" height="26" viewBox="0 0 24 24" fill="#113CCF"><path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"/></svg>'
-        },
-        amazon: {
-            name: 'Prime Video',
-            id: 9,
-            icon: '<svg width="26" height="26" viewBox="0 0 24 24" fill="#00A8E1"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2z"/></svg>'
-        },
-        apple: {
-            name: 'Apple TV+',
-            id: 350,
-            icon: '<svg width="26" height="26" viewBox="0 0 24 24" fill="#555"><path d="M17.05 20.28c-.98.95-2.05.8-3.08.35-1.09-.46-2.09-.48-3.24 0-1.44.62-2.2.44-3.06-.35C2.79 15.25 3.51 7.59 9.05 7.31c1.35.07 2.29.74 3.08.8 1.18-.24 2.31-.93 3.57-.84 1.51.12 2.65.72 3.4 1.8-3.12 1.87-2.38 5.98.48 7.13-.57 1.5-1.31 2.99-2.54 4.09z"/></svg>'
-        }
+    // Конфигурация сервисов
+    var services = {
+        netflix: { name: 'Netflix', id: 8 },
+        hbo: { name: 'HBO Max', id: 384 },
+        hulu: { name: 'Hulu', id: 15 },
+        disney: { name: 'Disney+', id: 337 },
+        amazon: { name: 'Prime Video', id: 9 },
+        apple: { name: 'Apple TV+', id: 350 }
     };
 
-    // Категории контента
-    var CATEGORIES = {
-        movies_popular: { 
-            name: 'Популярные фильмы', 
-            type: 'movie', 
-            sort: 'popularity.desc'
-        },
-        movies_new: { 
-            name: 'Новинки фильмов', 
-            type: 'movie', 
-            sort: 'primary_release_date.desc',
-            date_filter: true
-        },
-        series_popular: { 
-            name: 'Популярные сериалы', 
-            type: 'tv', 
-            sort: 'popularity.desc'
-        },
-        series_new: { 
-            name: 'Новинки сериалов', 
-            type: 'tv', 
-            sort: 'first_air_date.desc',
-            date_filter: true
-        }
-    };
+    // Категории
+    var categories = [
+        { id: 'movies_popular', name: 'Популярные фильмы', type: 'movie', sort: 'popularity.desc' },
+        { id: 'movies_new', name: 'Новинки фильмов', type: 'movie', sort: 'primary_release_date.desc', recent: true },
+        { id: 'series_popular', name: 'Популярные сериалы', type: 'tv', sort: 'popularity.desc' },
+        { id: 'series_new', name: 'Новинки сериалов', type: 'tv', sort: 'first_air_date.desc', recent: true }
+    ];
 
-    // Функция для создания URL запроса
-    function buildUrl(serviceId, category) {
-        var type = category.type;
-        var endpoint = type === 'movie' ? 'discover/movie' : 'discover/tv';
+    // Построение URL
+    function getUrl(serviceId, cat) {
+        var endpoint = cat.type === 'movie' ? 'discover/movie' : 'discover/tv';
+        var url = endpoint + '?sort_by=' + cat.sort;
+        url += '&with_watch_providers=' + serviceId;
+        url += '&watch_region=US';
+        url += '&vote_count.gte=20';
         
-        var params = {
-            sort_by: category.sort,
-            with_watch_providers: serviceId,
-            watch_region: 'US',
-            'vote_count.gte': 20
-        };
-
-        // Добавляем фильтр по дате для новинок
-        if (category.date_filter) {
-            var today = new Date();
-            var sixMonthsAgo = new Date(today.getFullYear(), today.getMonth() - 6, today.getDate());
-            var dateStr = sixMonthsAgo.toISOString().split('T')[0];
-            
-            if (type === 'movie') {
-                params['primary_release_date.gte'] = dateStr;
-            } else {
-                params['first_air_date.gte'] = dateStr;
-            }
+        if (cat.recent) {
+            var d = new Date();
+            d.setMonth(d.getMonth() - 6);
+            var date = d.toISOString().split('T')[0];
+            url += cat.type === 'movie' 
+                ? '&primary_release_date.gte=' + date
+                : '&first_air_date.gte=' + date;
         }
-
-        return Lampa.TMDB.api(endpoint, params);
+        
+        return url;
     }
 
     // Показ контента
-    function showContent(serviceKey, categoryKey) {
-        var service = STREAMING_SERVICES[serviceKey];
-        var category = CATEGORIES[categoryKey];
-        
-        var url = buildUrl(service.id, category);
+    function openContent(serviceKey, catIndex) {
+        var service = services[serviceKey];
+        var cat = categories[catIndex];
+        var url = getUrl(service.id, cat);
 
         Lampa.Activity.push({
             url: url,
-            title: service.name + ' - ' + category.name,
+            title: service.name + ' - ' + cat.name,
             component: 'category',
-            category: true,
+            page: 1,
             source: 'tmdb',
-            type: category.type,
-            page: 1
+            type: cat.type
         });
     }
 
-    // Показ меню категорий для сервиса
-    function showServiceMenu(serviceKey) {
-        var service = STREAMING_SERVICES[serviceKey];
+    // Меню выбора категории
+    function showCategories(serviceKey) {
+        var service = services[serviceKey];
         var items = [];
 
-        for (var catKey in CATEGORIES) {
+        for (var i = 0; i < categories.length; i++) {
             items.push({
-                title: CATEGORIES[catKey].name,
-                subtitle: '',
-                service: serviceKey,
-                category: catKey
+                title: categories[i].name,
+                index: i,
+                service: serviceKey
             });
         }
 
         Lampa.Select.show({
             title: service.name,
             items: items,
-            onSelect: function(a) {
-                showContent(a.service, a.category);
+            onSelect: function(item) {
+                openContent(item.service, item.index);
             },
             onBack: function() {
                 Lampa.Controller.toggle('menu');
@@ -142,42 +91,132 @@
         });
     }
 
-    // Добавление пункта в меню
-    function addMenuItem(serviceKey, serviceData) {
-        var item = $('<li class="menu__item selector" data-action="streaming_' + serviceKey + '">' +
-            '<div class="menu__ico">' + serviceData.icon + '</div>' +
-            '<div class="menu__text">' + serviceData.name + '</div>' +
-        '</li>');
+    // Компонент каталога
+    function component(serviceKey) {
+        var network = new Lampa.Reguest();
+        var scroll = new Lampa.Scroll({ mask: true, over: true });
+        var items = [];
+        var html = Lampa.Template.get('items_line', { title: services[serviceKey].name });
+        var body = html.find('.items-line__body');
+        var last;
 
-        item.on('hover:enter', function() {
-            showServiceMenu(serviceKey);
-        });
+        this.create = function() {
+            var _this = this;
+            
+            scroll.minus(html.find('.items-line__title'));
+            html.find('.items-line__title').text(services[serviceKey].name);
+            scroll.append(body);
+            
+            for (var i = 0; i < categories.length; i++) {
+                this.appendCategory(i);
+            }
 
-        $('.menu .menu__list').eq(0).append(item);
+            scroll.append(html);
+            this.activity.loader(false);
+            this.activity.toggle();
+        };
+
+        this.appendCategory = function(index) {
+            var cat = categories[index];
+            var card = Lampa.Template.get('card', { title: cat.name });
+            
+            card.addClass('card--category');
+            card.find('.card__img').css('background-color', '#353535');
+            card.find('.card__title').text(cat.name);
+            
+            card.on('hover:focus', function() {
+                last = card[0];
+                scroll.update(card, true);
+            });
+
+            card.on('hover:enter', function() {
+                openContent(serviceKey, index);
+            });
+
+            body.append(card);
+            items.push(card);
+        };
+
+        this.start = function() {
+            Lampa.Controller.add('content', {
+                toggle: function() {
+                    Lampa.Controller.collectionSet(scroll.render());
+                    Lampa.Controller.collectionFocus(last || false, scroll.render());
+                },
+                left: function() {
+                    if (Navigator.canmove('left')) Navigator.move('left');
+                    else Lampa.Controller.toggle('menu');
+                },
+                down: this.down,
+                up: this.up,
+                back: this.back
+            });
+
+            Lampa.Controller.toggle('content');
+        };
+
+        this.pause = function() {};
+        this.stop = function() {};
+        this.render = function() { return html; };
+        this.destroy = function() {
+            network.clear();
+            scroll.destroy();
+            html.remove();
+            items = null;
+            network = null;
+        };
     }
 
     // Инициализация
-    function init() {
-        // Добавляем пункты меню после загрузки интерфейса
+    function startPlugin() {
+        // Добавляем компонент для каждого сервиса
+        for (var key in services) {
+            (function(serviceKey) {
+                Lampa.Component.add('streaming_' + serviceKey, component.bind({}, serviceKey));
+            })(key);
+        }
+
+        // Добавляем в главное меню через Template
+        var menuAdded = false;
+        
         Lampa.Listener.follow('full', function(e) {
-            if (e.type == 'complite') {
-                setTimeout(function() {
-                    for (var key in STREAMING_SERVICES) {
-                        addMenuItem(key, STREAMING_SERVICES[key]);
+            if (e.type === 'complite' && !menuAdded) {
+                menuAdded = true;
+                
+                var menuList = document.querySelector('.menu .menu__list');
+                if (menuList) {
+                    for (var key in services) {
+                        (function(serviceKey, serviceName) {
+                            var li = document.createElement('li');
+                            li.className = 'menu__item selector';
+                            li.innerHTML = '<div class="menu__ico"><svg width="512" height="512" viewBox="0 0 24 24" fill="currentColor"><path d="M18 3v2h-2V3H8v2H6V3H4v18h2v-2h2v2h8v-2h2v2h2V3h-2zM8 17H6v-2h2v2zm0-4H6v-2h2v2zm0-4H6V7h2v2zm10 8h-2v-2h2v2zm0-4h-2v-2h2v2zm0-4h-2V7h2v2z"/></svg></div><div class="menu__text">' + serviceName + '</div>';
+                            
+                            li.addEventListener('click', function() {
+                                showCategories(serviceKey);
+                            });
+                            
+                            Lampa.Listener.follow('hover', function(ev) {
+                                if (ev.elem && ev.elem[0] === li && ev.type === 'enter') {
+                                    showCategories(serviceKey);
+                                }
+                            });
+                            
+                            menuList.appendChild(li);
+                        })(key, services[key].name);
                     }
-                }, 100);
+                }
             }
         });
 
-        console.log('[Plugin] ' + Plugin.name + ' v' + Plugin.version + ' загружен');
-        Lampa.Noty.show('Расширение "' + Plugin.name + '" активировано');
+        Lampa.Noty.show('Стриминговые сервисы загружены');
+        console.log('[Streaming Services] Плагин активирован');
     }
 
-    // Старт плагина
+    // Запуск
     if (window.Lampa) {
         Lampa.Listener.follow('app', function(e) {
-            if (e.type == 'ready') {
-                init();
+            if (e.type === 'ready') {
+                startPlugin();
             }
         });
     }
