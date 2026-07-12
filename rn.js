61b9dc952f64e025ad5ce0c073b9e5ee21a2a3fa
plitka-kran <plitka.kran@gmail.com>
2026-07-12 22:17:37 +0300

    Update playlist.js version to 1.0.13

--- rn.js
+++ rn.js
@@ -19,7 +19,7 @@
     // 	'online.js': '1.1', покане рабоатет 
         'pubtorr.js': '1.0',
         'ss.js': '1.0.1',
-	'playlist.js': '1.0.12',
+	'playlist.js': '1.0.13',
         'audio.js': '1.0.11'
 	};
 
