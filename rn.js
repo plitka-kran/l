86419c40e22952c5b0364def749e428cb41091be
plitka-kran <plitka.kran@gmail.com>
2026-07-14 22:33:33 +0300

    Update skipintro.js version to 1.0.20

--- rn.js
+++ rn.js
@@ -20,7 +20,7 @@
         'pubtorr.js': '1.0',
         'ss.js': '1.0.1',
         'playlist.js': '1.0.33',
-        'skipintro.js': '1.0.19',
+        'skipintro.js': '1.0.20',
         'audio.js': '1.0.11'
 	};
 
