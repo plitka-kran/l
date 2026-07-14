c48e6d3e356ca6aedc35974f52d42f7ccaf2717d
plitka-kran <plitka.kran@gmail.com>
2026-07-14 22:28:12 +0300

    Update rn.js

--- rn.js
+++ rn.js
@@ -20,7 +20,7 @@
         'pubtorr.js': '1.0',
         'ss.js': '1.0.1',
         'playlist.js': '1.0.33',
-        'skipintro.js': '1.0.17',
+        'skipintro.js': '1.0.18',
         'audio.js': '1.0.11'
 	};
 
