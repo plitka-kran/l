c1a6ad7a8ea7ed4c966fa4a3111910c59da641af
plitka-kran <plitka.kran@gmail.com>
2026-07-26 23:22:10 +0300

    Implement HDrezka login/logout and settings
    Added logging functionality and new settings for HDrezka integration.

--- online.js
+++ online.js
@@ -1535,7 +1535,13 @@
     var isLocal = !startsWith(window.location.protocol, 'http');
     var network = new Lampa.Reguest();
     var online_loading = false;
-
+    function logApp() {
+        console.log('Online Mod');
+        console.log('App', 'is MSX:', isMSX);
+        console.log('App', 'is Tizen:', isTizen);
+        console.log('App', 'is iframe:', isIFrame);
+        console.log('App', 'is local:', isLocal);
+    }
     function initStorage() {
         Lampa.Storage.set('online_mod_proxy_rezka2', 'false');
 
@@ -1591,6 +1597,9 @@
             online_mod_save_last_balanser: { ru: 'Сохранять историю балансеров', uk: 'Зберігати історію балансерів', be: 'Захоўваць гісторыю балансараў', en: 'Save history of balancers', zh: '保存平衡器的历史记录' },
             online_mod_clear_last_balanser: { ru: 'Очистить историю балансеров', uk: 'Очистити історію балансерів', be: 'Ачысціць гісторыю балансараў', en: 'Clear history of balancers', zh: '清除平衡器的历史记录' },
             online_mod_rezka2_mirror: { ru: 'Url HDrezka', uk: 'Url HDrezka', be: 'Url HDrezka', en: 'Url HDrezka', zh: 'Url HDrezka' },
+            online_mod_rezka2_login: { ru: 'Войти в HDrezka', uk: 'Увійти до HDrezka', be: 'Увайсці ў HDrezka', en: 'Log in to HDrezka', zh: '登录HDrezka' },
+            online_mod_rezka2_logout: { ru: 'Выйти из HDrezka', uk: 'Вийти з HDrezka', be: 'Выйсці з HDrezka', en: 'Log out of HDrezka', zh: '注销HDrezka' },
+            online_mod_secret_password: { ru: 'Секретный пароль', uk: 'Секретний пароль', be: 'Сакрэтны пароль', en: 'Secret password', zh: '秘密密码' },
             online_mod_rezka2_name: { ru: 'Логин или email для HDrezka', uk: 'Логін чи email для HDrezka', be: 'Лагін ці email для HDrezka', en: 'Login or email for HDrezka', zh: 'HDrezka的登录名或电子邮件' },
             online_mod_rezka2_password: { ru: 'Пароль для HDrezka', uk: 'Пароль для HDrezka', be: 'Пароль для HDrezka', en: 'Password for HDrezka', zh: 'HDrezka的密码' },
             online_mod_rezka2_cookie: { ru: 'Куки для HDrezka', uk: 'Кукі для HDrezka', be: 'Кукі для HDrezka', en: 'Cookie for HDrezka', zh: 'HDrezka 的 Cookie' },
@@ -1625,8 +1634,226 @@
             page: 1
         });
     }
+    function addSettingsOnlineMod() {
+        if (Lampa.Settings.main && Lampa.Settings.main() && !Lampa.Settings.main().render().find('[data-component="online_mod"]').length) {
+            var field = $(Lampa.Lang.translate("<div class=\"settings-folder selector\" data-component=\"online_mod\">\n            <div class=\"settings-folder__icon\">\n                <svg height=\"260\" viewBox=\"0 0 244 260\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n                <path d=\"M242,88v170H10V88h41l-38,38h37.1l38-38h38.4l-38,38h38.4l38-38h38.3l-38,38H204L242,88L242,88z M228.9,2l8,37.7l0,0 L191.2,10L228.9,2z M160.6,56l-45.8-29.7l38-8.1l45.8,29.7L160.6,56z M84.5,72.1L38.8,42.4l38-8.1l45.8,29.7L84.5,72.1z M10,88 L2,50.2L47.8,80L10,88z\" fill=\"white\"/>\n                </svg>\n            </div>\n            <div class=\"settings-folder__name\">#{online_mod_title_full}</div>\n        </div>"));
+            Lampa.Settings.main().render().find('[data-component="more"]').after(field);
+            Lampa.Settings.main().update();
+        }
+    }
 
+    function initSettings() {
+        var template = "<div>";
+
+        template += "\n        <div class=\"settings-param selector\" data-name=\"online_mod_prefer_http\" data-type=\"toggle\">\n            <div class=\"settings-param__name\">#{online_mod_prefer_http}</div>\n            <div class=\"settings-param__value\"></div>\n        </div>";
+        template += "\n        <div class=\"settings-param selector\" data-name=\"online_mod_full_episode_title\" data-type=\"toggle\">\n            <div class=\"settings-param__name\">#{online_mod_full_episode_title}</div>\n            <div class=\"settings-param__value\"></div>\n        </div>";
+        template += "\n        <div class=\"settings-param selector\" data-name=\"online_mod_save_last_balanser\" data-type=\"toggle\">\n            <div class=\"settings-param__name\">#{online_mod_save_last_balanser}</div>\n            <div class=\"settings-param__value\"></div>\n        </div>\n        <div class=\"settings-param selector\" data-name=\"online_mod_clear_last_balanser\" data-static=\"true\">\n            <div class=\"settings-param__name\">#{online_mod_clear_last_balanser}</div>\n            <div class=\"settings-param__status\"></div>\n        </div>";
+        template += "\n        <div class=\"settings-param selector\" data-name=\"online_mod_rezka2_mirror\" data-type=\"input\" placeholder=\"#{settings_cub_not_specified}\">\n            <div class=\"settings-param__name\">#{online_mod_rezka2_mirror}</div>\n            <div class=\"settings-param__value\"></div>\n        </div>";
+        template += "\n        <div class=\"settings-param selector\" data-name=\"online_mod_rezka2_name\" data-type=\"input\" placeholder=\"#{settings_cub_not_specified}\">\n            <div class=\"settings-param__name\">#{online_mod_rezka2_name}</div>\n            <div class=\"settings-param__value\"></div>\n        </div>\n        <div class=\"settings-param selector\" data-name=\"online_mod_rezka2_password\" data-type=\"input\" data-string=\"true\" placeholder=\"#{settings_cub_not_specified}\">\n            <div class=\"settings-param__name\">#{online_mod_rezka2_password}</div>\n            <div class=\"settings-param__value\"></div>\n        </div>";
+
+        if (Lampa.Platform.is('android')) {
+            Lampa.Storage.set("online_mod_rezka2_status", 'false');
+        } else {
+            template += "\n        <div class=\"settings-param selector\" data-name=\"online_mod_rezka2_login\" data-static=\"true\">\n            <div class=\"settings-param__name\">#{online_mod_rezka2_login}</div>\n            <div class=\"settings-param__status\"></div>\n        </div>\n        <div class=\"settings-param selector\" data-name=\"online_mod_rezka2_logout\" data-static=\"true\">\n            <div class=\"settings-param__name\">#{online_mod_rezka2_logout}</div>\n            <div class=\"settings-param__status\"></div>\n        </div>";
+        }
+
+        template += "\n        <div class=\"settings-param selector\" data-name=\"online_mod_rezka2_cookie\" data-type=\"input\" data-string=\"true\" placeholder=\"#{settings_cub_not_specified}\">\n            <div class=\"settings-param__name\">#{online_mod_rezka2_cookie}</div>\n            <div class=\"settings-param__value\"></div>\n        </div>\n        <div class=\"settings-param selector\" data-name=\"online_mod_rezka2_fill_cookie\" data-static=\"true\">\n            <div class=\"settings-param__name\">#{online_mod_rezka2_fill_cookie}</div>\n            <div class=\"settings-param__status\"></div>\n        </div>";
+        template += "\n        <div class=\"settings-param selector\" data-name=\"online_mod_secret_password\" data-type=\"input\" data-string=\"true\" placeholder=\"#{settings_cub_not_specified}\">\n            <div class=\"settings-param__name\">#{online_mod_secret_password}</div>\n            <div class=\"settings-param__value\"></div>\n        </div>";
+        template += "\n    </div>";
+
+        Lampa.Template.add('settings_online_mod', template);
+
+        if (window.appready) addSettingsOnlineMod();
+        else {
+            Lampa.Listener.follow('app', function (e) {
+                if (e.type == 'ready') addSettingsOnlineMod();
+            });
+        }
+
+        Lampa.Settings.listener.follow('open', function (e) {
+            if (e.name == 'online_mod') {
+                var clear_last_balanser = e.body.find('[data-name="online_mod_clear_last_balanser"]');
+                clear_last_balanser.unbind('hover:enter').on('hover:enter', function () {
+                    Lampa.Storage.set('online_mod_last_balanser', {});
+                    $('.settings-param__status', clear_last_balanser).removeClass('active error wait').addClass('active');
+                });
+
+                var rezka2_login = e.body.find('[data-name="online_mod_rezka2_login"]');
+                rezka2_login.unbind('hover:enter').on('hover:enter', function () {
+                    var rezka2_login_status = $('.settings-param__status', rezka2_login).removeClass('active error wait').addClass('wait');
+                    rezka2Login(function () {
+                        rezka2_login_status.removeClass('active error wait').addClass('active');
+                    }, function () {
+                        rezka2_login_status.removeClass('active error wait').addClass('error');
+                    });
+                });
+
+                var rezka2_logout = e.body.find('[data-name="online_mod_rezka2_logout"]');
+                rezka2_logout.unbind('hover:enter').on('hover:enter', function () {
+                    var rezka2_logout_status = $('.settings-param__status', rezka2_logout).removeClass('active error wait').addClass('wait');
+                    rezka2Logout(function () {
+                        rezka2_logout_status.removeClass('active error wait').addClass('active');
+                    }, function () {
+                        rezka2_logout_status.removeClass('active error wait').addClass('error');
+                    });
+                });
+
+                var rezka2_fill_cookie = e.body.find('[data-name="online_mod_rezka2_fill_cookie"]');
+                rezka2_fill_cookie.unbind('hover:enter').on('hover:enter', function () {
+                    var rezka2_fill_cookie_status = $('.settings-param__status', rezka2_fill_cookie).removeClass('active error wait').addClass('wait');
+                    rezka2FillCookie(function () {
+                        rezka2_fill_cookie_status.removeClass('active error wait').addClass('active');
+                        Lampa.Params.update(e.body.find('[data-name="online_mod_rezka2_cookie"]'), [], e.body);
+                    }, function () {
+                        rezka2_fill_cookie_status.removeClass('active error wait').addClass('error');
+                        Lampa.Params.update(e.body.find('[data-name="online_mod_rezka2_cookie"]'), [], e.body);
+                    });
+                });
+            }
+        });
+    }
+
+    // --- Rezka2 авторизация ---
+    function rezka2Login(success, error) {
+        var host = rezka2Mirror();
+        var url = host + '/ajax/login/';
+        var postdata = 'login_name=' + encodeURIComponent(Lampa.Storage.get('online_mod_rezka2_name', ''));
+        postdata += '&login_password=' + encodeURIComponent(Lampa.Storage.get('online_mod_rezka2_password', ''));
+        postdata += '&login_not_save=0';
+        network.clear();
+        network.timeout(8000);
+        network.silent(url, function (json) {
+            if (json && (json.success || json.message == 'Уже авторизован на сайте. Необходимо обновить страницу!')) {
+                Lampa.Storage.set('online_mod_rezka2_status', 'true');
+                network.clear();
+                network.timeout(8000);
+                network.silent(host + '/', function (str) {
+                    str = (str || '').replace(/\n/g, '');
+                    var error_form = str.match(/(<div class="error-code">[^<]*<div>[^<]*<\/div>[^<]*<\/div>)\s*(<div class="error-title">[^<]*<\/div>)/);
+                    if (error_form) {
+                        Lampa.Noty.show(error_form[0]);
+                        if (error) error();
+                        return;
+                    }
+                    var verify_form = str.match(/<span>MIRROR<\/span>.*<button type="submit" onclick="\$\.cookie(\([^)]*\))/);
+                    if (verify_form) {
+                        Lampa.Noty.show(Lampa.Lang.translate('online_mod_unsupported_mirror') + ' HDrezka');
+                        rezka2Logout(error, error);
+                        return;
+                    }
+                    if (success) success();
+                }, function (a, c) {
+                    if (success) success();
+                }, false, {
+                    dataType: 'text',
+                    withCredentials: true
+                });
+            } else {
+                Lampa.Storage.set('online_mod_rezka2_status', 'false');
+                if (json && json.message) Lampa.Noty.show(json.message);
+                if (error) error();
+            }
+        }, function (a, c) {
+            Lampa.Noty.show(network.errorDecode(a, c));
+            if (error) error();
+        }, postdata, {
+            withCredentials: true
+        });
+    }
+
+    function rezka2Logout(success, error) {
+        var url = rezka2Mirror() + '/logout/';
+        network.clear();
+        network.timeout(8000);
+        network.silent(url, function (str) {
+            Lampa.Storage.set('online_mod_rezka2_status', 'false');
+            if (success) success();
+        }, function (a, c) {
+            Lampa.Storage.set('online_mod_rezka2_status', 'false');
+            Lampa.Noty.show(network.errorDecode(a, c));
+            if (error) error();
+        }, false, {
+            dataType: 'text',
+            withCredentials: true
+        });
+    }
+
+    function rezka2FillCookie(success, error) {
+        var host = rezka2Mirror();
+        var url = host + '/ajax/login/';
+        var postdata = 'login_name=' + encodeURIComponent(Lampa.Storage.get('online_mod_rezka2_name', ''));
+        postdata += '&login_password=' + encodeURIComponent(Lampa.Storage.get('online_mod_rezka2_password', ''));
+        postdata += '&login_not_save=0';
+        network.clear();
+        network.timeout(8000);
+        network.silent(url, function (json) {
+            var cookie = '';
+            var values = {};
+            var sid = '';
+            if (!json.success) {
+                if (json.message) Lampa.Noty.show(json.message);
+                if (error) error();
+                return;
+            }
+            var cookieHeaders = json.headers && json.headers['set-cookie'] || null;
+            if (cookieHeaders && cookieHeaders.forEach) {
+                cookieHeaders.forEach(function (param) {
+                    var parts = param.split(';')[0].split('=');
+                    if (parts[0]) {
+                        if (parts[1] === 'deleted') delete values[parts[0]];
+                        else values[parts[0]] = parts[1] || '';
+                    }
+                });
+                sid = values['PHPSESSID'];
+                delete values['PHPSESSID'];
+                var cookies = [];
+                for (var name in values) {
+                    cookies.push(name + '=' + values[name]);
+                }
+                cookie = cookies.join('; ');
+            }
+            if (cookie) {
+                Lampa.Storage.set('online_mod_rezka2_cookie', cookie);
+                if (cookie.indexOf('PHPSESSID=') == -1) cookie = 'PHPSESSID=' + (sid || randomId2(26)) + (cookie ? '; ' + cookie : '');
+                network.clear();
+                network.timeout(8000);
+                network.silent(host + '/', function (str) {
+                    var body = (str || '').replace(/\n/g, '');
+                    var error_form = body.match(/(<div class="error-code">[^<]*<div>[^<]*<\/div>[^<]*<\/div>)\s*(<div class="error-title">[^<]*<\/div>)/);
+                    if (error_form) {
+                        Lampa.Noty.show(error_form[0]);
+                        if (error) error();
+                        return;
+                    }
+                    var verify_form = body.match(/<span>MIRROR<\/span>.*<button type="submit" onclick="\$\.cookie(\([^)]*\))/);
+                    if (verify_form) {
+                        Lampa.Storage.set('online_mod_rezka2_cookie', '');
+                        Lampa.Noty.show(Lampa.Lang.translate('online_mod_unsupported_mirror') + ' HDrezka');
+                        if (error) error();
+                        return;
+                    }
+                    if (success) success();
+                }, function (a, c) {
+                    if (success) success();
+                }, false, {
+                    dataType: 'text',
+                    withCredentials: true,
+                    headers: { Cookie: cookie }
+                });
+            } else {
+                if (error) error();
+            }
+        }, function (a, c) {
+            Lampa.Noty.show(network.errorDecode(a, c));
+            if (error) error();
+        }, postdata, {
+            withCredentials: true
+        });
+    }
+
+    // --- Запуск ---
     function startPlugin() {
+        logApp();
         initStorage();
         initLang();
         resetTemplates();
@@ -1651,8 +1878,8 @@
         };
         Lampa.Manifest.plugins = manifest;
 
-        var button = "<div class=\"full-start__button selector view--online_mod\" data-subtitle=\"\">\n        <svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"512\" height=\"512\" viewBox=\"0 0 244 260\" xml:space=\"preserve\"><g><path d=\"M242,88v170H10V88h41l-38,38h37.1l38-38h38.4l-38,38h38.4l38-38h38.3l-38,38H204L242,88L242,88z M228.9,2l8,37.7l0,0 L191.2,10L228.9,2z M160.6,56l-45.8-29.7l38-8.1l45.8,29.7L160.6,56z M84.5,72.1L38.8,42.4l38-8.1l45.8,29.7L84.5,72.1z M10,88 L2,50.2L47.8,80L10,88z\" fill=\"currentColor\"/></g></svg>\n        <span>#{online_mod_title}</span>\n        </div>";
-
+        var button = "<div class=\"full-start__button selector view--online_mod\" data-subtitle=\"\">\n        <svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" xmlns:svgjs=\"http://svgjs.com/svgjs\" version=\"1.1\" width=\"512\" height=\"512\" x=\"0\" y=\"0\" viewBox=\"0 0 244 260\" style=\"enable-background:new 0 0 512 512\" xml:space=\"preserve\" class=\"\">\n        <g xmlns=\"http://www.w3.org/2000/svg\">\n            <path d=\"M242,88v170H10V88h41l-38,38h37.1l38-38h38.4l-38,38h38.4l38-38h38.3l-38,38H204L242,88L242,88z M228.9,2l8,37.7l0,0 L191.2,10L228.9,2z M160.6,56l-45.8-29.7l38-8.1l45.8,29.7L160.6,56z M84.5,72.1L38.8,42.4l38-8.1l45.8,29.7L84.5,72.1z M10,88 L2,50.2L47.8,80L10,88z\" fill=\"currentColor\"/>\n        </g></svg>\n        <span>#{online_mod_title}</span>\n        </div>";
+        
         Lampa.Listener.follow('full', function (e) {
             if (e.type == 'complite') {
                 var btn = $(Lampa.Lang.translate(button));
@@ -1663,7 +1890,7 @@
                 e.object.activity.render().find('.view--torrent').after(btn);
             }
         });
+        initSettings();
     }
-
     startPlugin();
 })();
